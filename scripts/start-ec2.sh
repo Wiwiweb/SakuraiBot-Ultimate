369a9aa060f023724417763c9cd735b3f330322d
Wiwiweb <wiwiweb2@gmail.com>
2018-06-14 18:09:36 -0700

    Properly kill previous version

--- scripts/start-ec2.sh
+++ scripts/start-ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkill -f bot_manager.py
+pkill -f sakuraibot.py
 
 cd /home/ec2-user/SakuraiBot-Ultimate/src
 nohup python3 sakuraibot.py > /home/ec2-user/SakuraiBot-Ultimate/nohup.out 2>&1 &
